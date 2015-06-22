71573e5b3f8508dfbc53779c79d8b08ba761b64e
Chirag Aggarwal <chi6rag@gmail.com>
2015-06-22 16:38:54 +0530

    Specify orientation; specify config changes; handle screen orientation change

---
+++ app/src/main/java/net/chi6rag/android/orientationtest/MainActivity.java
@@ -0,0 +1,50 @@
+package net.chi6rag.android.orientationtest;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d("chi6rag", "" +
+                (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT ?
+                        "Welcome to Portrait Mode" : "Welcome to landscape Mode")
+        );
+        Log.d("chi6rag", "Orientation Changed");
+    }
+}
